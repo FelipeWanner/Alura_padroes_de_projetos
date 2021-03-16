1701c864261acb26fd9d366ece103d3b2fabbd47
Felipe Rafael Wanner <felipe.w.pakua@gmail.com>
2021-03-16 14:52:14 -0300

    adding DateHelper to the index and NegociacaoController files

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -15,21 +15,13 @@ class NegociacaoController {
     adiciona(event){
         event.preventDefault();
 
-        //let data = new Date(this._inputData.value.replace(/-/g, ","));
-        //Método funcional mais simples, porém para praticar outras coisas no curso, utilizar o código abaixo
-        let data = new Date(
-            ...this._inputData.value
-            .split("-")
-            .map((item, indice) => item - indice % 2)           
-        );
-
         let negociacao = new Negociacao (
-            data, 
+            DateHelper.textoParaData(this._inputData.value), 
             this._inputQuantidade.value,
             this._inputValor.value
         );
 
-
         console.log(negociacao);
+        console.log(DateHelper.dataParaTexto(negociacao.data));
     }
 }
